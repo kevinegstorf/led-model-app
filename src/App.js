77a3d9a8bc1ab3e23a11f48e78c8e61a39a635b7
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2020-02-04 13:35:23 +0100

    remove scroll

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from "react";
 import Box from "./Box";
 import "./App.css";
 
+function useLockBodyScroll() {
+  useLayoutEffect(() => {
+    // Get original body overflow
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    // Prevent scrolling on mount
+    document.body.style.overflow = "hidden";
+    // Re-enable scrolling when component unmounts
+    return () => (document.body.style.overflow = originalStyle);
+  }, []); // Empty array ensures effect is only run on mount and unmount
+}
+
 function App() {
+  useLockBodyScroll();
   const boxArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
   return (
     <div className="App">
